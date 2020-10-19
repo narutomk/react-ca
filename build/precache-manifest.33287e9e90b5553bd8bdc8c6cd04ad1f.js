89ed1f90c409fcef0c5ba2404223a225d4de30da
narutomk <khaled.bouflija@gmail.com>
2020-10-19 14:35:03 +0100

    MySecondCommit

---
+++ build/precache-manifest.33287e9e90b5553bd8bdc8c6cd04ad1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0da5d71e752321c4eef65e614961015",
+    "url": "/react-ca/index.html"
+  },
+  {
+    "revision": "895e7e317334f27fa8f5",
+    "url": "/react-ca/static/css/main.62927a30.chunk.css"
+  },
+  {
+    "revision": "cddfd08082f2f28533ae",
+    "url": "/react-ca/static/js/2.126bf728.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-ca/static/js/2.126bf728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "895e7e317334f27fa8f5",
+    "url": "/react-ca/static/js/main.dde1b1b9.chunk.js"
+  },
+  {
+    "revision": "069e7edf1a9ea6e15cd9",
+    "url": "/react-ca/static/js/runtime-main.0d184c98.js"
+  }
+]);
